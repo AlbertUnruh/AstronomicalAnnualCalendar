10308bf0739d91905ad4f801f512a5e52169b794
AlbertUnruh <73029826+AlbertUnruh@users.noreply.github.com>
2024-10-11 00:10:00 +0200

    fix not all arguments showing up during initialization

--- AstronomicalAnnualCalendar/__main__.py
+++ AstronomicalAnnualCalendar/__main__.py
@@ -1,9 +1,22 @@
 # third party
 import click
 
+# local
+from .enums import ObservableObjectEnum
+
 
 def main():  # noqa: D103
     click.secho("Not implemented yet...", err=True, fg="red")
+    click.secho(
+        f"Debug output: "
+        f"\n{ObservableObjectEnum.SUN.internal_id=!r}"
+        f"\n{ObservableObjectEnum.SUN.name=!r}"
+        f"\n{ObservableObjectEnum.SUN.localized_name=!r}"
+        f"\n{ObservableObjectEnum.SUN.is_sun=!r}"
+        f"\n{ObservableObjectEnum.SUN.line_color=!r}"
+        f"\n{ObservableObjectEnum.SUN.line_strength=!r}",
+        fg="green",
+    )
 
 
 if __name__ == "__main__":
--- AstronomicalAnnualCalendar/models.py
+++ AstronomicalAnnualCalendar/models.py
@@ -1,6 +1,9 @@
 # third party
 from annotated_types import LowerCase
-from pydantic import BaseModel, PositiveFloat
+from pydantic import BaseModel
+from pydantic.config import ConfigDict
+from pydantic.fields import Field
+from pydantic.types import PositiveFloat
 from pydantic_extra_types.color import Color
 
 
@@ -11,10 +14,12 @@ _SUN_LINE_STRENGTH_MULTIPLIER: float = 2
 
 
 class ObservableObjectModel(BaseModel):  # noqa: D101  # ToDo: add documentation
+    model_config = ConfigDict(frozen=True)
+
     internal_id: LowerCase
     line_color: Color
-    _is_sun: bool = None  # ToDo: find way to have it appear available in init
-    _line_strength: PositiveFloat = 2
+    is_sun_: bool = Field(default=None, alias="is_sun")
+    line_strength_: PositiveFloat = Field(default=2, alias="line_strength")
     """NOTE: if the object is a sun the line_strength will get modified!"""
 
     @property
@@ -35,11 +40,11 @@ class ObservableObjectModel(BaseModel):  # noqa: D101  # ToDo: add documentation
         It's determined by setting ``is_sun`` to either True or False.
         If not set will check whether the ``internal_id`` equals "sun".
         """
-        if self._is_sun is None:
+        if self.is_sun_ is None:
             return self.internal_id == "sun"
-        return self._is_sun
+        return self.is_sun_
 
     @property
     def line_strength(self) -> PositiveFloat:
         """Returns the appropriate line-strength for the object."""
-        return self._line_strength * (_SUN_LINE_STRENGTH_MULTIPLIER if self.is_sun else 1)
+        return self.line_strength_ * (_SUN_LINE_STRENGTH_MULTIPLIER if self.is_sun else 1)
