9b89408120ac02f004496ef0e5da02c2031e5c0d
AlbertUnruh <73029826+AlbertUnruh@users.noreply.github.com>
2024-10-17 13:54:29 +0200

    add tests for ``.is_moon`` and ``.is_planet``

--- tests/test_models.py
+++ tests/test_models.py
@@ -35,3 +35,40 @@ def test_oom_name():
 )
 def test_oom_is_sun(oom: ObservableObjectModel, expected: bool):
     assert oom.is_sun == expected
+
+
+@pytest.mark.parametrize(
+    "oom, expected",
+    [
+        (ObservableObjectModel(id="oom", line_color=Color("000")), False),
+        (ObservableObjectModel(id="oom", line_color=Color("000"), is_moon=False), False),
+        (ObservableObjectModel(id="oom", line_color=Color("000"), is_moon=True), True),
+        (ObservableObjectModel(id="moon", line_color=Color("000")), True),
+        (ObservableObjectModel(id="moon", line_color=Color("000"), is_moon=False), False),
+        (ObservableObjectModel(id="moon", line_color=Color("000"), is_moon=True), True),
+        (ObservableObjectModel(id="moonshine", line_color=Color("000")), False),
+    ],
+)
+def test_oom_is_moon(oom: ObservableObjectModel, expected: bool):
+    assert oom.is_moon == expected
+
+
+@pytest.mark.parametrize(
+    "oom, expected",
+    [
+        (ObservableObjectModel(id="oom", line_color=Color("000")), True),
+        (ObservableObjectModel(id="oom", line_color=Color("000"), is_planet=False), False),
+        (ObservableObjectModel(id="oom", line_color=Color("000"), is_planet=True), True),
+        (ObservableObjectModel(id="planet", line_color=Color("000")), True),
+        (ObservableObjectModel(id="sun", line_color=Color("000")), False),
+        (ObservableObjectModel(id="moon", line_color=Color("000")), False),
+        (ObservableObjectModel(id="planet", line_color=Color("000"), is_planet=False), False),
+        (ObservableObjectModel(id="planet", line_color=Color("000"), is_planet=True), True),
+        (ObservableObjectModel(id="planet", line_color=Color("000"), is_sun=True), False),
+        (ObservableObjectModel(id="planet", line_color=Color("000"), is_moon=True), False),
+        (ObservableObjectModel(id="planetarium", line_color=Color("000")), True),
+        (ObservableObjectModel(id="something", line_color=Color("000")), True),
+    ],
+)
+def test_oom_is_planet(oom: ObservableObjectModel, expected: bool):
+    assert oom.is_planet == expected
