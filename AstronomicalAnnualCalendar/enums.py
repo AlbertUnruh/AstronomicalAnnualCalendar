298e41854ccea806f85cc2f099525d2dfb6e8944
AlbertUnruh <73029826+AlbertUnruh@users.noreply.github.com>
2024-10-19 16:11:57 +0200

    update (add) aliases for observable objects

--- AstronomicalAnnualCalendar/enums.py
+++ AstronomicalAnnualCalendar/enums.py
@@ -7,7 +7,7 @@ from aenum import EnumMeta, IntFlag, NoAliasEnum, auto
 from pydantic_extra_types.color import Color
 
 # local
-from .models import ObservableObjectModel
+from .models import ObservableObjectModel as OOModel
 
 
 __all__ = (
@@ -40,17 +40,17 @@ class ObservableObjectEnum(NoAliasEnum, metaclass=DirectValueMeta):
     *in case the download gets removed: https://web.archive.org/web/20240531110805/https://sternwarte-papenburg.de/jahreskalender/download/ajk_2024.pdf
     """
 
-    SUN: ObservableObjectModel = ObservableObjectModel(id="sun", line_color=Color("orange"))
-    MERCURY: ObservableObjectModel = ObservableObjectModel(id="mercury", line_color=Color("black"))
-    VENUS: ObservableObjectModel = ObservableObjectModel(id="venus", line_color=Color("green"))
-    MOON: ObservableObjectModel = ObservableObjectModel(id="moon", line_color=Color("violet"))
+    SUN: OOModel = OOModel(id="sun", aliases={"Sonne"}, line_color=Color("orange"))
+    MERCURY: OOModel = OOModel(id="mercury", aliases={"Merkur"}, line_color=Color("black"))
+    VENUS: OOModel = OOModel(id="venus", aliases={"Venus"}, line_color=Color("green"))
+    MOON: OOModel = OOModel(id="moon", aliases={"Mond"}, line_color=Color("violet"))
     # ^^^ EXPERIMENTAL; may get excluded if it's to wonky ^^^
-    MARS: ObservableObjectModel = ObservableObjectModel(id="mars", line_color=Color("red"))
-    JUPITER: ObservableObjectModel = ObservableObjectModel(id="jupiter", line_color=Color("blue"))
-    SATURN: ObservableObjectModel = ObservableObjectModel(id="saturn", line_color=Color("pink"))
-    URANUS: ObservableObjectModel = ObservableObjectModel(id="uranus", line_color=Color("turquoise"))
+    MARS: OOModel = OOModel(id="mars", aliases={"Mars"}, line_color=Color("red"))
+    JUPITER: OOModel = OOModel(id="jupiter", aliases={"Jupiter"}, line_color=Color("blue"))
+    SATURN: OOModel = OOModel(id="saturn", aliases={"Saturn"}, line_color=Color("pink"))
+    URANUS: OOModel = OOModel(id="uranus", aliases={"Uranus"}, line_color=Color("turquoise"))
     # ^^^ rename to "Urectum" in 2620 (https://futurama.fandom.com/wiki/Urectum) ^^^
-    NEPTUNE: ObservableObjectModel = ObservableObjectModel(id="neptune", line_color=Color("gold"))
+    NEPTUNE: OOModel = OOModel(id="neptune", aliases={"Neptun"}, line_color=Color("gold"))
 
 
 class AntiIntFlag[T: int]:
