c00bbf9411863043c77c8bfd8136694f386cc50d
AlbertUnruh <73029826+AlbertUnruh@users.noreply.github.com>
2024-11-24 19:19:25 +0100

    populate `__init__.py` with metadata

---
+++ AstronomicalAnnualCalendar/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.0"
+__description__ = "A tool to help to generate the astronomical annual calendar"
+__license__ = "MIT"
+__authors__ = ["AlbertUnruh <AlbertUnruh@pm.me>"]
+__repository__ = "https://github.com/AlbertUnruh/AstronomicalAnnualCalendar/"
