13867b0ed8d655bec97db16aef9390385ba4ba56
AlbertUnruh <73029826+AlbertUnruh@users.noreply.github.com>
2024-12-10 17:59:44 +0100

    reorganize file-structure for sample-data

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,57 +14,57 @@ _BASE_PATH: Path = Path(__file__).parent
 @fixture
 def path_complete_10d() -> Path:
     """Every object; 10-day interval; limited calculations;"""
-    return _BASE_PATH / Path("sample_data/complete-10d.txt")
+    return _BASE_PATH / Path("sample_data/10d/complete.txt")
 
 
 @fixture
 def path_mercury_10d() -> Path:
     """Mercury; 10-day interval; limited calculations;"""
-    return _BASE_PATH / Path("sample_data/mercury-10d.txt")
+    return _BASE_PATH / Path("sample_data/10d/mercury.txt")
 
 
 @fixture
 def path_neptune_1d() -> Path:
     """Neptune; 1-day interval; limited calculations;"""
-    return _BASE_PATH / Path("sample_data/neptune-1d.txt")
+    return _BASE_PATH / Path("sample_data/1d/neptune.txt")
 
 
 @fixture
 def path_neptune_10d() -> Path:
     """Neptune; 10-day interval; limited calculations;"""
-    return _BASE_PATH / Path("sample_data/neptune-10d.txt")
+    return _BASE_PATH / Path("sample_data/10d/neptune.txt")
 
 
 @fixture
 def path_sun_10d() -> Path:
     """Sun; 10-day interval; limited calculations;"""
-    return _BASE_PATH / Path("sample_data/sun-10d.txt")
+    return _BASE_PATH / Path("sample_data/10d/sun.txt")
 
 
 @fixture
 def path_sun_moon_mercury_10d_everything() -> Path:
     """Sun, moon and mercury; 10-day interval; all calculations;"""
-    return _BASE_PATH / Path("sample_data/sun,moon,mercury-10d-everything.txt")
+    return _BASE_PATH / Path("sample_data/10d/sun,moon,mercury-everything.txt")
 
 
 @fixture
 def path_sun_d1_2_everything() -> Path:
-    return _BASE_PATH / Path("sample_data/sun-d1+2-everything.txt")
+    return _BASE_PATH / Path("sample_data/d1+2/sun-d1+2.txt")
 
 
 @fixture
 def path_moon_d1_2_everything() -> Path:
-    return _BASE_PATH / Path("sample_data/moon-d1+2-everything.txt")
+    return _BASE_PATH / Path("sample_data/d1+2/moon-d1+2.txt")
 
 
 @fixture
 def path_saturn_d1_2_everything() -> Path:
-    return _BASE_PATH / Path("sample_data/saturn-d1+2-everything.txt")
+    return _BASE_PATH / Path("sample_data/d1+2/saturn-d1+2.txt")
 
 
 @fixture
 def path_sun_moon_saturn_d1_2_everything() -> Path:
-    return _BASE_PATH / Path("sample_data/sun,moon,saturn-d1+2-everything.txt")
+    return _BASE_PATH / Path("sample_data/d1+2/sun,moon,saturn-d1+2.txt")
 
 
 @fixture(scope="session")
